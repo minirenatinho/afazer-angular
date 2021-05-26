faf9c9376a7d8636f4b0fe5acebfdcfc7728c9a4
Mini Renatinho <minirenatinhopequeno@gmail.com>
2021-05-25 22:30:23 -0300

    changing api url to heroku host

--- src/app/afazer-api.service.ts
+++ src/app/afazer-api.service.ts
@@ -8,23 +8,26 @@ import { Item } from './item';
   providedIn: 'root'
 })
 export class AfazerApiService {
-  apiUrl = 'http://localhost:3000/api/items';
+  apiUrl = 'https://afazer-api.herokuapp.com/api/items';
+  apiUrlDev = 'http://localhost:3000/api/items';
+  userId = '5d3f4c7ab72b5a001727317a';
+  userToken = '1d58c5b5-66d9-43d9-92db-c799c1d79363';
 
   constructor(private http: HttpClient) { }
 
   listItems() {
-    return this.http.get<ListItemsResponse>(this.apiUrl, { headers: new HttpHeaders({'id': '5d3f4c7ab72b5a001727317a', 'token': '1d58c5b5-66d9-43d9-92db-c799c1d79363'})});
+    return this.http.get<ListItemsResponse>(this.apiUrl, { headers: new HttpHeaders({'id': this.userId, 'token': this.userToken})});
   }
 
   addItem(item: Item) {
-    return this.http.post(this.apiUrl, item, { headers: new HttpHeaders({'id': '5d3f4c7ab72b5a001727317a', 'token': '1d58c5b5-66d9-43d9-92db-c799c1d79363'})});
+    return this.http.post(this.apiUrl, item, { headers: new HttpHeaders({'id': this.userId, 'token': this.userToken})});
   }
 
   editItem(item: Item) {
-    return this.http.patch(this.apiUrl+'/'+item._id, item, { headers: new HttpHeaders({'id': '5d3f4c7ab72b5a001727317a', 'token': '1d58c5b5-66d9-43d9-92db-c799c1d79363'})});
+    return this.http.patch(this.apiUrl+'/'+item._id, item, { headers: new HttpHeaders({'id': this.userId, 'token': this.userToken})});
   }
 
   deleteItem(itemId: string) {
-    return this.http.delete(this.apiUrl+'/'+itemId, { headers: new HttpHeaders({'id': '5d3f4c7ab72b5a001727317a', 'token': '1d58c5b5-66d9-43d9-92db-c799c1d79363'})});
+    return this.http.delete(this.apiUrl+'/'+itemId, { headers: new HttpHeaders({'id': this.userId, 'token': this.userToken})});
   }
 }
